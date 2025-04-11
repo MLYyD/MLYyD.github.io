b1ea07c907d6c1b292add962ebef048241ad2379
unknown <2822361857@qq.com>
2025-04-11 10:20:25 +0800

    Site updated: 2025-04-11 10:20:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/10/jiangjia/","2025/04/10/caiwei/"];function toRandomPost(){
+var posts=["2025/04/10/intr/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
